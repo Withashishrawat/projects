408ceb70c599ee10a7de2ac6c44882ee13e763e0
Withashishrawat <rawatashish1010@gmail.com>
2025-06-24 23:29:42 +0530

    Add files via upload

---
+++ domProjects.js
@@ -0,0 +1,36 @@
+let buttons=document.querySelectorAll(".btn");
+buttons.forEach(function (e){
+    e.addEventListener("click",handling)
+});
+
+function handling(e){
+// console.log( e.target);
+// if(e.target.id==="blue"){
+// document.body.style.backgroundColor=e.target.id;
+// }
+// if(e.target.id==="black"){
+// document.body.style.backgroundColor=e.target.id;
+// }
+// if(e.target.id==="yellow"){
+// document.body.style.backgroundColor=e.target.id;
+// }
+// if(e.target.id==="red"){
+// document.body.style.backgroundColor=e.target.id;
+// }
+// if(e.target.id==="purple"){
+// document.body.style.backgroundColor=e.target.id;
+// }
+// let color = e.target.id;
+
+//   switch (color) {
+//     case "blue":
+//     case "black":
+//     case "yellow":
+//     case "red":
+//     case "purple":
+//       document.body.style.backgroundColor = color;
+//       break;
+//     default:
+//       console.log("Unknown color:", color);
+// }
+};
